e6491d3857652560df4397d9ca7d3a8d7c452aa0
Wasiq Zhakfar <wasiqzhakfar@gmail.com>
2022-04-25 00:14:51 +0500

    FA19-BSE-220

---
+++ app/src/main/java/com/masub/fa19_bse_220_assignment_2/Home_Page.java
@@ -0,0 +1,14 @@
+package com.masub.fa19_bse_220_assignment_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
